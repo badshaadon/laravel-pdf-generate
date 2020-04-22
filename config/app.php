0eabe7829fe3f8ffbdc3a1f7624997c76699563d
Ali Naeem <alitheflyingtiger111@gmail.com>
2020-04-22 07:29:39 +0500

    Service provider and aliases are added in conifg file

--- config/app.php
+++ config/app.php
@@ -174,7 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        /*
+            PDF Generate
+        */
+        Barryvdh\DomPDF\ServiceProvider::class,
     ],
 
     /*
@@ -226,7 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
